982ad2be147c4966398a678810130f48ec8e0315
navindu amarakoon <navanav3@gmail.com>
2020-02-27 00:10:20 +0530

    Fixed SVGs to correct sizes

--- client/Tabs.js
+++ client/Tabs.js
@@ -4,9 +4,9 @@ function renderTabs(app) {
   //---------------------------- Vision -----------------------------
   //icon
   visionIcon = PIXI.Sprite.from(app.loader.resources.vision.texture);
-  visionIcon.scale.set(0.7);
+  //visionIcon.scale.set(0.7);
   visionIcon.anchor.set(0.5);
-  visionIcon.x = app.view.width / 2 - 220;
+  visionIcon.x = app.view.width / 2 - 500;
   visionIcon.y = app.loader.resources.vision.texture.height / 2 + 100;
   //click handler
   visionIcon.buttonMode = true;
@@ -17,25 +17,24 @@ function renderTabs(app) {
   });
   app.stage.addChild(visionIcon);
   //text
-  visionText = new PIXI.Text("Vision");
+  visionText = new PIXI.Text("VISION");
   visionText.anchor.set(0.5);
   visionText.y = visionIcon.y - visionIcon.texture.height / 2 - 20;
   visionText.x = visionIcon.x;
   app.stage.addChild(visionText);
   //frame
   visionFrame = PIXI.Sprite.from(app.loader.resources.visionFrame.texture);
-  visionFrame.scale.set(0.7);
+  //visionFrame.scale.set(0.7);
   visionFrame.anchor.set(0.5, 0);
   visionFrame.x = app.view.width / 2;
-  visionFrame.y = 220;
+  visionFrame.y = 250;
   app.stage.addChild(visionFrame);
 
   //---------------------------- Solution ----------------------------
   //icon
   solutionIcon = PIXI.Sprite.from(app.loader.resources.solution.texture);
-  solutionIcon.scale.set(0.7);
   solutionIcon.anchor.set(0.5);
-  solutionIcon.x = app.view.width / 2 - 110;
+  solutionIcon.x = app.view.width / 2 - 250;
   solutionIcon.y = app.loader.resources.vision.texture.height / 2 + 100;
   //click handler
   solutionIcon.buttonMode = true;
@@ -46,24 +45,22 @@ function renderTabs(app) {
   });
   app.stage.addChild(solutionIcon);
   //text
-  solutionText = new PIXI.Text("Solution");
+  solutionText = new PIXI.Text("SOLUTION");
   solutionText.anchor.set(0.5);
   solutionText.y = solutionIcon.y - solutionIcon.texture.height / 2 - 20;
   solutionText.x = solutionIcon.x;
   app.stage.addChild(solutionText);
   //frame
   solutionFrame = PIXI.Sprite.from(app.loader.resources.solutionFrame.texture);
-  solutionFrame.scale.set(0.7);
   solutionFrame.anchor.set(0.5, 0);
   solutionFrame.x = app.view.width / 2;
-  solutionFrame.y = 220;
+  solutionFrame.y = 250;
   app.stage.addChild(solutionFrame);
 
   //---------------------------- Story ----------------------------
   //icon
   storyIcon = PIXI.Sprite.from(app.loader.resources.story.texture);
   storyIcon.anchor.set(0.5);
-  storyIcon.scale.set(0.7);
   storyIcon.x = app.view.width / 2;
   storyIcon.y = app.loader.resources.vision.texture.height / 2 + 100;
   //click handler
@@ -75,25 +72,23 @@ function renderTabs(app) {
   });
   app.stage.addChild(storyIcon);
   //text
-  storyText = new PIXI.Text("Story");
+  storyText = new PIXI.Text("STORY");
   storyText.anchor.set(0.5);
   storyText.y = storyIcon.y - storyIcon.texture.height / 2 - 20;
   storyText.x = storyIcon.x;
   app.stage.addChild(storyText);
   //frame
   storyFrame = PIXI.Sprite.from(app.loader.resources.storyFrame.texture);
-  storyFrame.scale.set(0.7);
   storyFrame.anchor.set(0.5, 0);
   storyFrame.x = app.view.width / 2;
-  storyFrame.y = 220;
+  storyFrame.y = 250;
   app.stage.addChild(storyFrame);
 
-  //---------------------------- Milestones ----------------------------
+  //---------------------------- Milestones / goal----------------------------
   //icon
   milestonesIcon = PIXI.Sprite.from(app.loader.resources.milestones.texture);
   milestonesIcon.anchor.set(0.5);
-  milestonesIcon.scale.set(0.7);
-  milestonesIcon.x = app.view.width / 2 + 110;
+  milestonesIcon.x = app.view.width / 2 + 250;
   milestonesIcon.y = app.loader.resources.vision.texture.height / 2 + 100;
   //click handler
   milestonesIcon.buttonMode = true;
@@ -104,7 +99,7 @@ function renderTabs(app) {
   });
   app.stage.addChild(milestonesIcon);
   //text
-  milestonesText = new PIXI.Text("Milestones");
+  milestonesText = new PIXI.Text("GOALS");
   milestonesText.anchor.set(0.5);
   milestonesText.y = milestonesIcon.y - milestonesIcon.texture.height / 2 - 20;
   milestonesText.x = milestonesIcon.x;
@@ -113,18 +108,16 @@ function renderTabs(app) {
   milestonesFrame = PIXI.Sprite.from(
     app.loader.resources.milestonesFrame.texture,
   );
-  milestonesFrame.scale.set(0.7);
   milestonesFrame.anchor.set(0.5, 0);
   milestonesFrame.x = app.view.width / 2;
-  milestonesFrame.y = 220;
+  milestonesFrame.y = 250;
   app.stage.addChild(milestonesFrame);
 
   //---------------------------- Join Us ----------------------------
   //icon
   joinUsIcon = PIXI.Sprite.from(app.loader.resources.joinUs.texture);
   joinUsIcon.anchor.set(0.5);
-  joinUsIcon.scale.set(0.7);
-  joinUsIcon.x = app.view.width / 2 + 220;
+  joinUsIcon.x = app.view.width / 2 + 500;
   joinUsIcon.y = app.loader.resources.vision.texture.height / 2 + 100;
   //click handler
   joinUsIcon.buttonMode = true;
@@ -135,17 +128,16 @@ function renderTabs(app) {
   });
   app.stage.addChild(joinUsIcon);
   //text
-  joinUsText = new PIXI.Text("Join Us");
+  joinUsText = new PIXI.Text("JOIN US");
   joinUsText.anchor.set(0.5);
   joinUsText.y = joinUsIcon.y - joinUsIcon.texture.height / 2 - 20;
   joinUsText.x = joinUsIcon.x;
   app.stage.addChild(joinUsText);
   //frame
   joinusFrame = PIXI.Sprite.from(app.loader.resources.joinUsFrame.texture);
-  joinusFrame.scale.set(0.7);
   joinusFrame.anchor.set(0.5, 0);
   joinusFrame.x = app.view.width / 2;
-  joinusFrame.y = 220;
+  joinusFrame.y = 250;
   app.stage.addChild(joinusFrame);
 
   //----------------------------------------------------------
@@ -202,5 +194,7 @@ function renderTabs(app) {
         joinusFrame.visible = true;
         renderScreen(app, "joinus");
     }
+
+    app.view.height = 3000;
   }
 }
