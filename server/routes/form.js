e907a4fbaad66678c3b02089e47de29fa46f2824
Injae Cho <injae.cho3139@gmail.com>
2020-02-26 11:43:31 -0500

    added dev form

--- server/routes/form.js
+++ server/routes/form.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const genericForm = require("../models/generic");
+const devForm = require("../models/dev");
 
 router.post("/newgenericform", (req, res) => {
   const form = req.body;
@@ -15,6 +16,35 @@ router.post("/newgenericform", (req, res) => {
     Is_Partner: form.Is_Partner,
     Is_Player: form.Is_Player,
     Newsletter: form.Newsletter,
+  });
+
+  newForm.save(e => {
+    if (e) {
+      console.error("error while saving form to database", e);
+      res.sendStatus(500);
+    } else {
+      res.json("Submission Success!");
+    }
+  });
+});
+
+router.post("/newdevform", (req, res) => {
+  const form = req.body;
+
+  const newForm = new devForm({
+    Name: form.Name,
+    Email: form.Email,
+    Professional: form.Professional,
+    Student: form.Student,
+    Related_Org: form.Related_Org,
+    Experience: form.Experience,
+    Stack: form.Stack,
+    Why_Build: form.Why_Build,
+    Want_Build: form.Want_Build,
+    Game_Jam: form.Game_Jam,
+    Game_Jam_details: form.Game_Jam_Details,
+    Reason_no_game_jam: form.Reason_no_game_jam,
+    Classification: form.Classification,
   });
 
   newForm.save(e => {
